16c80985b4fa96b02e7db8f7786c04521bc6c9d0
Maxim <shidloba@gmail.com>
2023-07-08 21:58:20 +0300

    create repository

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -2,14 +2,14 @@ import pool from '../db'
 import { iUser } from '../interfaces/index';
 
 
-async function getUserByIdDB(id: number): Promise <iUser[]> {
+async function getUserByIdDB(id: number): Promise<iUser[]> {
     const client = await pool.connect();
     const sql = 'SELECT * FROM users WHERE id = $1';
     const result = (await client.query(sql, [id])).rows;
     return result;
 }
 
-async function getAllUsersDB(): Promise <iUser[]> {
+async function getAllUsersDB(): Promise<iUser[]> {
     const client = await pool.connect();
     const sql = 'SELECT * FROM users';
     const result = (await client.query(sql)).rows;
@@ -30,6 +30,19 @@ async function deleteUserByIdDB(id: number): Promise<iUser[]> {
         const sql = `DELETE FROM users WHERE id = $1 RETURNING *`;
         const result = (await client.query(sql, [id])).rows;
         await client.query(`COMMIT`)
+        return result;
+    } catch (error) {
+        await client.query('ROLLBACK');
+        return [];
+    }
+}
+async function updateUserByIdDB(id: number, name: string, surname: string, email: string, pwd: string): Promise<iUser[]> {
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const sql = `UPDATE users SET name = $1, surname = $2, email=$3, pwd=$4 WHERE id = $5 RETURNING *`;
+        const result = (await client.query(sql, [name, surname, email, pwd, id])).rows;
+        await client.query('COMMIT');
         return result;
     } catch (error) {
         await client.query('ROLLBACK');
