3d806e42e0512a549baac48a9de6addfcefab19f
Maxim <shidloba@gmail.com>
2023-07-10 20:04:21 +0300

    add buildResponse

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 import bodyParser from "body-parser";
 import user from "../src/controller/user.controller";
 
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/user', user)
 
-app.use((err, req, res, _nest) => {
+app.use((err, req:Request, res:Response, _nest:NextFunction) => {
     res.send(err.message);
 })
 export default app;
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,28 +1,29 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import { createUser, getAllUsers, getUserById } from '../service/user.service';
+import {buildResponse} from '../helper/buildResponse';
 
 const route = express.Router();
 
-route.get('/', async (req, res,): Promise<void> => {
+route.get('/', async (req:Request, res:Response): Promise<void> => {
     try {
         const data = await getAllUsers()
         buildResponse(res, 200, data)
     } catch (err: any) {
-        res.send(err.message)
+        buildResponse(res, 404, err.message)
     }
 })
 
-route.get(`/:id`, async (req, res): Promise<void> => {
+route.get(`/:id`, async (req:Request, res:Response): Promise<void> => {
     try {
         const { id } = req.params;
         const data = await getUserById(id);
         buildResponse(res, 200, data)
     } catch (err: any) {
-        res.send(err.message)
+        buildResponse(res, 404, err.message)
     }
 })
 
-route.post('/', async (req, res): Promise<void> => {
+route.post('/', async (req:Request, res:Response): Promise<void> => {
     try {
         const { name, surname, email, pwd } = req.body
         const data = await createUser(name, surname, email, pwd)
