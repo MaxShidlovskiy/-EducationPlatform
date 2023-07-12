f11808e4c302ebcb96cada6abefda29929190f82
Maxim <shidloba@gmail.com>
2023-07-12 21:34:32 +0300

    create get request in course

--- src/controller/course.controller.ts
+++ src/controller/course.controller.ts
@@ -1,11 +1,25 @@
+import express, { Request, Response } from 'express'
+import { getCourse, getCourseById, createCourse, deleteCourseById } from '../service/course.service'
+import buildResponse from '../helper/buildResponse';
+import isValidCourseBody from '../helper/validation';
 
 const course = express.Router();
 
-course.get('/', async (req:Request, res:Response)=>{
+course.get('/', async (req: Request, res: Response) => {
     try {
         const data = await getCourse();
+        buildResponse(res, 200, data);
+    } catch (error: any) {
+        buildResponse(res, 400, error.message)
+    }
+});
+
+course.get('/:id', async (req:Request, res:Response)=>{
+    try {
+        const {id} = req.params;
+        const data = await getCourseById(id);
         buildResponse(res, 200, data);
     } catch (error:any) {
         buildResponse(res, 400, error.message)
     }
-})
+});
