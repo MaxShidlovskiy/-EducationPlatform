255c3f9a6ffdc0b2c9ed1356027443780e44e14a
Maxim <shidloba@gmail.com>
2023-07-12 21:30:35 +0300

    create api files

--- src/controller/api.controller.ts
+++ src/controller/api.controller.ts
@@ -1,14 +1,26 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import buildResponse from '../helper/buildResponse';
-import {registration, authorizationUser} from '../service/api.service';
+import { registration, authorizationUser } from '../service/api.service';
 const api = express.Router();
 
-api.post('/registration', async (req:Request, res:Response):Promise<void>=>{
+api.post('/registration', async (req: Request, res: Response): Promise<void> => {
     try {
-        const{name, surname, email, pwd} = req.body;
+        const { name, surname, email, pwd } = req.body;
         const data = await registration(name, surname, email, pwd);
         buildResponse(res, 200, data);
-    } catch (error:any) {
+    } catch (error: any) {
         buildResponse(res, 400, error.message);
     }
-})
+});
+
+api.post('/auth', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { name, surname, email, pwd } = req.body;
+        const data = await authorizationUser(email, pwd);
+        buildResponse(res, 200, data);
+    } catch (error: any) {
+        buildResponse(res, 400, error.message);
+    }
+});
+
+export default api;
