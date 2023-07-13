b8b80bcbb7292cef3748d8c796e3635c9cb9c950
Maxim <shidloba@gmail.com>
2023-07-13 21:35:24 +0300

    create delete in course

--- src/controller/course.controller.ts
+++ src/controller/course.controller.ts
@@ -23,3 +23,13 @@ course.get('/:id', async (req:Request, res:Response)=>{
         buildResponse(res, 400, error.message)
     }
 });
+
+course.delete('/:id', async (req:Request, res:Response)=>{
+    try {
+        const {id} = req.params;
+        const data = await deleteCourseById(id);
+        buildResponse(res, 200, data);
+    } catch (error:any) {
+        buildResponse(res, 400, error.message)
+    }
+});
