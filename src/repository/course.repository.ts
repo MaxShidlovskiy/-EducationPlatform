cc8f3fe11c7c8ea1e64d7e7296800ac6f74aed3d
Maxim <shidloba@gmail.com>
2023-07-16 22:13:10 +0300

    add delete in course repository

--- src/repository/course.repository.ts
+++ src/repository/course.repository.ts
@@ -1,25 +1,32 @@
 import pool from "../db"
-import {iCourse} from "../interfaces/index"
+import { iCourse } from "../interfaces/index"
 
-async function getCourseDB():Promise<iCourse[]>{
+async function getCourseDB(): Promise<iCourse[]> {
     const client = await pool.connect();
     const sql = `SELECT * FROM courses`;
     const result = (await client.query(sql)).rows;
     return result;
 }
 
-async function getCourseByIdDB(id):Promise<iCourse[]>{
+async function getCourseByIdDB(id): Promise<iCourse[]> {
     const client = await pool.connect();
     const sql = `SELECT * FROM courses WHERE id=$1`;
-    const data = (await client.query(sql,[id])).rows;
+    const data = (await client.query(sql, [id])).rows;
     return data;
 }
 
-async function deleteCourseByIdDB(id):Promise<iCourse[]>{
+async function deleteCourseByIdDB(id): Promise<iCourse[]> {
     const client = await pool.connect();
     const sql = `DELETE FROM courses WHERE id=$1 RETURNING *`;
-    const data = (await client.query(sql,[id])).rows;
+    const data = (await client.query(sql, [id])).rows;
     return data;
 }
 
-export {getCourseDB
+async function createCourseDB(course: string): Promise<iCourse[]> {
+    const client = await pool.connect();
+    const sql = `INSERT INTO courses (course) VALUES ($1) RETURNING *`;
+    const data = (await client.query(sql, [course])).rows;
+    return data;
+}
+
+export { getCourseDB, getCourseByIdDB, createCourseDB, deleteCourseByIdDB }
