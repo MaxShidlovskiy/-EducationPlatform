73fa9a52daa1acba684b7ba031bec6b2a7adab52
Maxim <shidloba@gmail.com>
2023-07-20 09:54:09 +0300

    create course test file

--- tests/servise/course.test.ts
+++ tests/servise/course.test.ts
@@ -1,4 +1,4 @@
-import { createCourse, getAllCourse, getCourseById } from '../../src/service/course.service';
+import { createCourse, getAllCourse, getCourseById, updateCourse, deleteCourseById } from '../../src/service/course.service';
 import * as repository from '../../src/repository/course.repository';
 
 describe('createCourse', () => {
@@ -11,8 +11,20 @@ describe('createCourse', () => {
         expect(funcRepo).toHaveBeenCalled();
         expect(result).toEqual([{ id: 1, course: 'course' }]);
         expect(result).toHaveLength(1);
+
+    })
+    test('error', async () => {
+        const funcRepo = jest.spyOn(repository, 'createCourseDB');
+        funcRepo.mockResolvedValue([]);
+        try {
+          await createCourse('course');
+        } catch (error:any) {
+            expect(funcRepo).toHaveBeenCalled();
+            expect(error.message).toBe(`курс не сохранен`);
+        }
     })
 })
+
 describe('getAllCourse', () => {
     test('seccess', async () => {
         const funcRepo = jest.spyOn(repository, 'getCourseDB');
@@ -39,3 +51,28 @@ describe('getCourseById', () => {
     });
 });
 
+describe('updateCourse function', () => {
+    test('success', async () => {
+        const funcRepo = jest.spyOn(repository, 'updateCourseDB');
+        funcRepo.mockResolvedValue([{ id: 1, course: 'test_course1' }]);
+
+        const result = await updateCourse(1, 'test_course1');
+
+        expect(funcRepo).toHaveBeenCalled();
+        expect(result).toEqual([{ id: 1, course: 'test_course1' }]);
+        expect(result.length).toBeGreaterThan(0)
+    })
+})
+
+describe('deleteCourseById', () => {
+    test('seccess', async () => {
+        const funcRepo = jest.spyOn(repository, 'deleteCourseByIdDB');
+        funcRepo.mockResolvedValue([{ id: 1, course: 'course1' }]);
+
+        const result = await deleteCourseById(1)
+
+        expect(funcRepo).toHaveBeenCalled();
+        expect(result).toEqual([{ id: 1, course: 'course1' }]);
+        expect(result.length).toBeGreaterThan(0);
+    })
+})
