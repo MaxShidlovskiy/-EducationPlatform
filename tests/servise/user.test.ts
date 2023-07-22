daef6d84ad51814e721d1a0642f033c3351f3651
Maxim <shidloba@gmail.com>
2023-07-22 10:52:07 +0300

    update user.test

--- tests/servise/user.test.ts
+++ tests/servise/user.test.ts
@@ -1,5 +1,30 @@
-import { createUser, getAllUsers } from '../../src/service/user.service';
+import { createUser, getAllUsers, getUserById, updateUserById } from '../../src/service/user.service';
 import * as repository from '../../src/repository/user.repository';
+
+describe(`createUserNewFunction`, () => {
+    test(`success`, async () => {
+        const funcRepo = jest.spyOn(repository, 'createUserDB');
+        funcRepo.mockResolvedValue([
+            {
+                "id": 1,
+                "name": "test",
+                "surname": "test",
+                "email": "test@mail.com",
+                "pwd": "1234"
+            }
+        ])
+        const result = await createUser("test", "test", "test@mail.com", "1234")
+        expect(result).toEqual([{
+            "id": 1,
+            "name": "test",
+            "surname": "test",
+            "email": "test@mail.com",
+            "pwd": "1234"
+        }])
+        expect(result.length).toBe(1);
+        expect(funcRepo).toHaveBeenCalled();
+    })
+})
 
 describe('createUser', () => {
     test('seccess', async () => {
@@ -12,7 +37,7 @@ describe('createUser', () => {
             "pwd": "1234"
         }]);
 
-        const result = await createUser("TestName", "TestSurname", "test@gmail.com", "123");
+        const result = await createUser("test", "test", "test@mail.com", "1234");
 
         expect(funcRepo).toHaveBeenCalled();
         expect(result).toEqual(
@@ -27,17 +52,17 @@ describe('createUser', () => {
             ]);
         expect(result).toHaveLength(1);
     })
-})
 
-test('error', async () => {
-    const functRepo = jest.spyOn(repository, "createUserDB");
-    functRepo.mockResolvedValue([]);
-    try {
-        const result = await createUser("test", "test", "test@mail.com", "1234");
-    } catch (error: any) {
-        expect(functRepo).toHaveBeenCalled();
-        expect(error.message).toBe('empty user')
-    }
+    test('error', async () => {
+        const functRepo = jest.spyOn(repository, "createUserDB");
+        functRepo.mockResolvedValue([]);
+        try {
+            const result = await createUser("test", "test", "test@mail.com", "1234");
+        } catch (error: any) {
+            expect(functRepo).toHaveBeenCalled();
+            expect(error.message).toBe('empty user')
+        }
+    })
 })
 
 describe('getAllUsers', () => {
@@ -70,3 +95,53 @@ describe('getAllUsers', () => {
         expect(result.length).toBeGreaterThan(0)
     });
 });
+
+describe('getUserById', () => {
+    test('success', async () => {
+        const funcRepo = jest.spyOn(repository, 'getUserByIdDB');
+        funcRepo.mockResolvedValue([{
+            "id": 1,
+            "name": "test",
+            "surname": "test",
+            "email": "test@mail.com",
+            "pwd": "1234"
+        }])
+
+        const result = await getUserById(1);
+
+        expect(funcRepo).toHaveBeenCalled();
+        expect(result).toEqual([{
+            "id": 1,
+            "name": "test",
+            "surname": "test",
+            "email": "test@mail.com",
+            "pwd": "1234"
+        }]);
+        expect(result.length).toBeGreaterThan(0);
+    });
+});
+
+describe('updateUserById', () => {
+    test('success', async () => {
+        const funcRepo = jest.spyOn(repository, 'updateUserByIdDB');
+        funcRepo.mockResolvedValue([{
+            "id": 1,
+            "name": "test",
+            "surname": "test",
+            "email": "test@mail.com",
+            "pwd": "1234"
+        }]);
+
+        const result = await updateUserById(1, "test", "test", "test@mail.com", "1234");
+
+        expect(funcRepo).toHaveBeenCalled();
+        expect(result).toEqual([{
+            "id": 1,
+            "name": "test",
+            "surname": "test",
+            "email": "test@mail.com",
+            "pwd": "1234"
+        }]);
+        expect(result.length).toBeGreaterThan(0)
+    })
+})
