978aed80da6f288557d4733e474212373535f631
Maxim <shidloba@gmail.com>
2023-07-30 09:57:03 +0300

    update app js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import HomePage from './Pages/HomePage/HomePage'
 import { Routes, Route } from 'react-router-dom'
 import RegPage from './Pages/RegPage/RegPage';
 import AuthPage from './Pages/AuthPage/AuthPage';
+import StudentPage from './Pages/StudentPage/StudentPage';
+import CoursePage from './Pages/CoursePage/CoursePage';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Route path='/' element={<HomePage />}></Route>
       <Route path='/reg' element={<RegPage />}></Route>
       <Route path='/auth' element={<AuthPage />}></Route>
+      <Route path='/student' element={<StudentPage />}></Route>
+      <Route path='/course/:id' element={<CoursePage />}></Route>
     </Routes>
 
   );
