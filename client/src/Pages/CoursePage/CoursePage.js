b77a3da1768f3a068e34e148eb41c3f4f498ad11
Maxim <shidloba@gmail.com>
2023-08-02 10:05:22 +0300

    repair course page

--- client/src/Pages/CoursePage/CoursePage.js
+++ client/src/Pages/CoursePage/CoursePage.js
@@ -3,21 +3,35 @@ import { useEffect, useState } from "react";
 import array from "../../Storage/course.json"
 import Header from "../../companents/Header/Header"
 import Footer from "../../companents/Footer/Footer";
+import style from './style.module.css';
+
 
 export default function CoursePage() {
-    const { id } = useParams();
-    const [elem, setElem] = useState({});
+    const { id } = useParams()
+    const [value, setValue] = useState({})
     useEffect(() => {
-        const curElem = array.filter((el) => el.id === id)
-        setElem(curElem[0])
+        const currentEl = array.filter((el) => el.id === id)
+        setValue(currentEl[0])
     }, [id])
+
     return (
         <>
             <Header />
             <div className={style.container}>
-                
+                <div className={style.course}>
+                    <div className={style.flex}>
+                        <div className={style.img}></div>
+                        <div className={style.informationCourse}>
+                            <h2>{value.name}</h2>
+                            <p>{value.text}</p>
+                        </div>
+                    </div>
+                    <div className={style.btn}>Go to Course</div>
+                </div>
+
+                <div className={style.lessons}></div>
             </div>
-            <Footer></Footer>
+            <Footer />
         </>
     )
 }
