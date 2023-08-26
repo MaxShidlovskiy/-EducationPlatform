29425db70c29d78ab42836e696e63804a88e44bc
Maxim <shidloba@gmail.com>
2023-08-26 09:55:36 +0300

    update client files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import HomePage from './Pages/homepages/HomePage'
+import HomePage from './Pages/HomePage/HomePage'
+import { Routes, Route } from 'react-router-dom'
+import RegPage from './Pages/RegPage/RegPage';
+import AuthPage from './Pages/AuthPage/AuthPage';
+
 
 function App() {
   return (
-     <div >
-    <HomePage/>
-    </div>  
+    <Routes>
+      <Route path='/' element={<HomePage />}></Route>
+      <Route path='/reg' element={<RegPage />}></Route>
+      <Route path='/auth' element={<AuthPage />}></Route>
+    </Routes>
+
   );
 }
 
