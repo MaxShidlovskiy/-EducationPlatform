dc52b6d0c9e8c2a8092236e5d217bf17b5e8a0b3
Maxim <shidloba@gmail.com>
2023-08-28 10:02:24 +0300

    create regpage

---
+++ client/src/Pages/RegPage/RegPage.js
@@ -0,0 +1,42 @@
+import Header from '../../companents/Header/Header'
+import Footer from '../../companents/Footer/Footer'
+import style from './style.module.css'
+import Input from '../../companents/Input/Input';
+import { useState } from 'react';
+import axios from 'axios'
+
+function RegPage() {
+  const [value, setValue] = useState({ name: '', surname: '', email: '', pwd: '' })
+
+  const arr = ['name', 'surname', 'email', 'pwd']
+
+  async function show() {
+    debugger
+    const result = await axios.post('http://localhost:3001/api/registration', value)
+    console.log(result);
+  }
+
+  return (
+    <div>
+      < Header />
+
+      <div className={style.wrapper}>
+
+        <div className={style.signUpInfo}>
+
+          <h1>Sign Up</h1>
+          <Input arr={arr} value={value} setValue={setValue} />
+          <div className={style.wrapperBtn} onClick={show}>Sign Up</div>
+
+        </div >
+
+        <div className={style.signUpImg}></div>
+
+      </div >
+
+      <Footer />
+    </div >
+  );
+}
+
+export default RegPage;
