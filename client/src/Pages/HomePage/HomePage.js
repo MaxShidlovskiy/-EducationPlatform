02fd4c329bb121b2d7ab410206c1a1194e1691ab
Maxim <shidloba@gmail.com>
2023-08-28 10:04:59 +0300

    update homepage

--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import Header from "../../companents/Header/Header"
 import Preview from "../../companents/Preview/Preview"
+import Footer from "../../companents/Footer/Footer"
 
 function HomePage() {
     return (
         <div>
             <Header />
             <Preview />
+            <Footer />
         </div>
     )
 }
