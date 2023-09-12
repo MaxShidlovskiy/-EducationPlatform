c0dc1c86fcdfb9fdb7a15571d809a623252e1911
Maxim <shidloba@gmail.com>
2023-09-12 11:31:21 +0300

    create lesson repository

--- server/src/repository/lesson.repository.ts
+++ server/src/repository/lesson.repository.ts
@@ -1,13 +1,28 @@
-import pool from '../db';
+import pool from "../db";
 
-async function createLessonDB(title: string, courseId: number) {
-    console.log('+');
+async function createLessonDB(title:string, courseId:number){
     const client = await pool.connect();
-    console.log(client);
-
-    const sql = `INSERT ITNO lessons(title, courseId) VALUES ($1,$2) returning*`
-    const data = (await client.query(sql, [title, courseId])).rows;
+    const sql = `INSERT INTO lessons(title, courseId) VALUES ($1, $2) RETURNING * `;
+    const data = (await client.query(sql,[title, courseId])).rows;
+    return data;
+}
+async function updateLessonDB(title:string, courseId:number, id:number){
+    const client = await pool.connect();
+    const sql = `UPDATE lessons SET title=$1, courseId=$2  WHERE id=$3 RETURNING * `;
+    const data = (await client.query(sql,[title, courseId, id])).rows;
+    return data;
+}
+async function getLessonByCourseIdDB(courseId:number){
+    const client = await pool.connect();
+    const sql = `SELECT * FROM lessons WHERE courseId=$1 `;
+    const data = (await client.query(sql,[courseId])).rows;
+    return data;
+}
+async function deleteLessonDB(id:number){
+    const client = await pool.connect();
+    const sql = `DELETE FROM lessons WHERE id=$1 RETURNING *`;
+    const data = (await client.query(sql,[id])).rows;
     return data;
 }
 
-export { createLessonDB }
+export {createLessonDB, updateLessonDB, getLessonByCourseIdDB, deleteLessonDB}
