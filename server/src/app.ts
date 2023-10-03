1ceaf1c60a9f19d4bb5cc1d0cf3f2bd819f5d7ba
Maxim <shidloba@gmail.com>
2023-10-03 10:28:12 +0300

    create app file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,18 @@
 import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
 import bodyParser from "body-parser";
 import user from "../src/controller/user.controller";
+import cors from "cors";
 import course from './controller/course.controller';
 import api from "./controller/api.controller";
+import lesson from './controller/lesson.controller';
 
 const app = express();
 app.use(cors())
 app.use(bodyParser.json());
-app.use('/user', user)
-app.use('/course', course)
-app.use('/api', api)
-
+app.use('/user', user);
+app.use('/course', course);
+app.use('/api', api);
+app.use('/lesson', lesson)
 
 app.use((err, req: Request, res: Response, _nest: NextFunction) => {
     res.send(err.message);
